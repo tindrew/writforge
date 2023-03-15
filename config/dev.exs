4dab014266d2cecd13c455c8f2cc2e3f696a1642
tindrew <tindrewmar@gmail.com>
2023-03-15 16:04:23 -0500

    initial commit for group blog project

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :blog, BlogWeb.Endpoint,
   secret_key_base: "WuXuZTTlsDkGvkhZS+0dRaJFqRP5bkMurhg9cbPT4MynZvIWdVLnwNEVIh5ZJDqs",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
