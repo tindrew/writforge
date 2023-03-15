ee52a95d0d4be9e798e5d16cc2cc5e4243c6127d
tindrew <tindrewmar@gmail.com>
2023-03-15 16:27:40 -0500

    add dialyxir

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule Blog.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
