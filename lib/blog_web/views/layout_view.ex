57b120e32273cb53b4e49c8dd28b701d69247b16
tindrew <tindrewmar@gmail.com>
2023-03-17 00:47:47 -0500

    added nav bar links as components

--- lib/blog_web/views/layout_view.ex
+++ lib/blog_web/views/layout_view.ex
@@ -1,6 +1,6 @@
 defmodule BlogWeb.LayoutView do
   use BlogWeb, :view
-
+  alias BlogWeb.Components
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
