5d6afcc9704a7f3042db9915af64458346fca836
tindrew <tindrewmar@gmail.com>
2023-03-29 13:49:03 -0500

    started on adding-tags

--- lib/blog_web/components.ex
+++ lib/blog_web/components.ex
@@ -3,7 +3,7 @@ defmodule BlogWeb.Components do
 
   def link(assigns) do
     ~H"""
-      <a href={@href} class="sm:px-6 text-gray-600 duration-300  hover:border-b-4 border-white-400"><%= render_slot(@inner_block) %></a>
+      <a href={@href} class="sm:px-8 rounded m-2 px-4 py-2 bg-gray-300 hover:bg-gray-500 transition-all duration-300"><%= render_slot(@inner_block) %></a>
     """
   end
 end
